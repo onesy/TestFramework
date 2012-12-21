f559cd9236d53cf221b471dcf3bab58d7215c0bd
onesy <onesy@JMCDYFPC0030.(none)>
2012-12-21 19:13:26 +0800

    create the new born project

---
+++ org/KKframework/mainField/KKFrameworkLoader.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of KKFrameworkLoader
+ *
+ * @author onesy
+ */
+class KKFrameworkFileLoader {
+    //put your code here
+}
+
+?>
